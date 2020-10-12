0e0cac0b0b00ffaffab6d7e1758c5d71bb34914d
Loudcow67 <logan.soulliere@ontariotechu.net>
2020-10-12 14:47:30 -0400

    Saving and Loading Added

---
+++ Assets/Scrpits/SaveableObject.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum ObjectType { Cube, Cylinder, Sphere}
+
+public abstract class SaveableObject : MonoBehaviour
+{
+    protected string save;
+
+    [SerializeField]
+    private ObjectType objectType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SaveGameManager.Instance.SaveableObjects.Add(this);
+
+    }
+
+    public virtual void Save (int id)
+    {
+        PlayerPrefs.SetString(id.ToString(), objectType + "_" + transform.position.ToString());
+    }
+
+    public virtual void Load(string[] values)
+    {
+        transform.localPosition = SaveGameManager.Instance.StringToVector(values[1]);
+    }
+
+    public void DestroySaveable()
+    {
+
+    }
+}
